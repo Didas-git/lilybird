3756a48567247460b0ea3cf85a2a438507b59845
DidaS <didasoficial@gmail.com>
2023-12-02 18:48:34 +0000

    Run biome

--- packages/test/globals.d.ts
+++ packages/test/globals.d.ts
@@ -1,7 +1,7 @@
 declare module "bun" {
     interface Env {
-        TOKEN: string,
-        TEST_CHANNEL_ID: string
-        TEST_GUILD_ID: string
+        TOKEN: string;
+        TEST_CHANNEL_ID: string;
+        TEST_GUILD_ID: string;
     }
 }
