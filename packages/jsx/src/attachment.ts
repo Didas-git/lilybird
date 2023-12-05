d87d9f9fffaff1a7711389d89acbda3629b746b5
DidaS <didasoficial@gmail.com>
2023-12-05 05:22:35 +0000

    Added `uri` prop to `LilybirdAttachment`

--- packages/jsx/src/attachment.ts
+++ packages/jsx/src/attachment.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import type { LilybirdAttachment } from "lilybird";
 
-export function Attachment(props: LilybirdAttachment | { path: string, name: string }): LilybirdAttachment {
+export function Attachment(props: LilybirdAttachment | { path: string, name: string }): LilybirdAttachment & { uri: string } {
     return {
         file: "path" in props ? Bun.file(props.path) : props.file,
-        name: props.name
+        name: props.name,
+        uri: `attachment://${props.name}`
     };
 }
