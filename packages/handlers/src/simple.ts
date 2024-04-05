0865767d407da2f8f429eb572365e957d063753b
DidaS <didasoficial@gmail.com>
2024-04-05 04:46:49 +0100

    Move exports

--- packages/handlers/src/simple.ts
+++
@@ -1,5 +0,0 @@
-export type * from "./simple/message-commands.js";
-export type * from "./simple/slash-command.js";
-export type * from "./simple/events.js";
-
-export * from "./simple/handler.js";
---
+++ packages/handlers/src/simple/index.ts
@@ -0,0 +1,5 @@
+export type * from "./message-commands.js";
+export type * from "./slash-command.js";
+export type * from "./events.js";
+
+export * from "./handler.js";
