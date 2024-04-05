cf707d8dcdcf25082e8ba97323432f145ea6c030
DidaS <didasoficial@gmail.com>
2024-04-05 03:10:57 +0100

    Add redis caching module

--- build.sh
+++ build.sh
@@ -10,13 +10,18 @@ echo Building lilybird transformers module
 cd ../transformers
 bun run build
 
+echo Building lilybird handlers module
+cd ../handlers
+bun run build
+
+echo Building lilybird redis module
+cd ../redis
+bun run build
+
 echo Building lilybird jsx module
 cd ../jsx
 bun run build
 
-echo Building lilybird handlers module
-cd ../handlers
-bun run build
 
 echo Building create-bot package
 cd ../create
