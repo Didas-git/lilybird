d118319e2f1016ee6ab9e6dcb42dba64272bf357
DidaS <didasoficial@gmail.com>
2024-05-05 17:05:19 +0100

    Use enum to get event name
    `GatewayEvent` is not a `const enum` anymore but it shouldn't affect performance since listeners are compiled using the literals either way

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -176,12 +176,13 @@ export class Client<T extends Transformers = Transformers, C extends CacheManage
         //#endregion
         //#region User defined listeners
         for (let i = 0, listenerEntries = Object.entries(listeners), { length } = listenerEntries; i < length; i++) {
-            const [name, handler] = listenerEntries[i] as [string, () => unknown];
-            if (name === "raw" || name === "ready") continue;
+            const [handlerName, handler] = listenerEntries[i] as [string, () => unknown];
+            if (handlerName === "raw" || handlerName === "ready") continue;
             if (typeof handler === "undefined") continue;
 
-            const event = name.replace(/[A-Z]/, "_$&").toUpperCase() as GatewayEvent;
-            const transformer = transformers[name];
+            const name = `${handlerName[0].toUpperCase()}${handlerName.slice(1)}`;
+            const event: GatewayEvent = GatewayEvent[<never>name];
+            const transformer = transformers[handlerName];
 
             this.#createListener(
                 builder,
