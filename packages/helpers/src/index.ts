ff53c2223a8ec460d62552adb1e2ec00b205ff4f
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-10 18:57:58 +0600

    Use `helpers` module instead of `jsx`

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./message-formatting.js";
 export * from "./markdown.js";
 export * from "./webhook.js";
+export * from "./colors.js";
--- packages/jsx/src/embed.ts
+++ packages/jsx/src/embed.ts
@@ -3,44 +3,6 @@ import { EmbedType } from "lilybird";
 
 import type { Embed } from "lilybird";
 
-export enum Color {
-    Red = 0xFF0000,
-    Green = 0x00FF00,
-    Blue = 0x0000FF,
-    Yellow = 0xFFFF00,
-    Purple = 0x800080,
-    Violet = 0xEE82EE,
-    Indigo = 0x4B0082,
-    SpringGreen = 0x00FF7F,
-    FernGreen = 0x4F7942,
-    Crimson = 0xDC143C,
-    Golden = 0xFFD700,
-    Pink = 0xFFC0CB,
-    LimeGreen = 0x32CD32,
-    WaterBlue = 0x3399FF,
-    WaterGreen = 0x3EB489,
-    SkyBlue = 0x87CEEB,
-    Mint = 0x98FF98,
-    Orange = 0xFFA500,
-    BananaYellow = 0xFFFACD,
-    Peach = 0xFFDAB9,
-    KiwiGreen = 0x8EE53F,
-    BerryPurple = 0x990099,
-    LemonYellow = 0xFFF44F,
-    GrapePurple = 0x6F2DA8
-}
-
-function parseHexToByte10(hex: string): number {
-    if (!hex.startsWith("#")) hex = `#${hex}`;
-    return Number(hex.replace("#", "0x"));
-}
-
-function rgbToByte10(rgb: Array<number>): number {
-    if (rgb.length === 3) return Number(`0x${((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1)}`);
-    else if (rgb.length === 4) return Number(`0x${((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1)}`); // Because hex doesn't have opacity.
-    throw new RangeError(`Invalid RGB/RGBA length. Expected: 3 or 4. Got: ${rgb.length}`);
-}
-
 function parseEmbedChildren(children: Array<EmbedComponent> | EmbedComponent | undefined): Omit<Embed.Structure, "title" | "type" | "description" | "url" | "timestamp" | "color"> | undefined {
     if (children == null) return;
     if (!Array.isArray(children)) return { [children.type]: children.data };
@@ -78,14 +40,11 @@ export function Embed({
     description?: string,
     url?: string,
     timestamp?: boolean | Date | number,
-    color?: Color | string | number | Array<number>,
+    color?: number,
     children?: Array<EmbedComponent> | EmbedComponent
 }): Embed.Structure {
     if (typeof timestamp === "number") timestamp = new Date(timestamp);
     if (typeof timestamp === "boolean") timestamp = new Date();
-
-    if (typeof color === "string") color = parseHexToByte10(color);
-    else if (Array.isArray(color)) color = rgbToByte10(color);
 
     return {
         title,
