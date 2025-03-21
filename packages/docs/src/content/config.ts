a0a6442952b10d394591b87e712179bd2f58a7e9
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-03-22 00:52:36 +0300

    chore: upgraded astro to v5

--- packages/docs/src/content/config.ts
+++ packages/docs/src/content/config.ts
@@ -1,8 +1,8 @@
 import { defineCollection } from "astro:content";
-import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
+import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders';
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = <unknown>{
-    docs: defineCollection({ schema: docsSchema() }),
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-    i18n: defineCollection({ type: "data", schema: i18nSchema() })
+    docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+    i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() })
 };
